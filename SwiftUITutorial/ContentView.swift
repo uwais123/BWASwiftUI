1ee81fb3e74a25e9dfe4ec6190b4cd037948837d
uwaisalqadri <uwaisalqadri654321@gmail.com>
2020-12-01 23:02:24 +0700

    update

--- SwiftUITutorial/ContentView.swift
+++ SwiftUITutorial/ContentView.swift
@@ -4,22 +4,88 @@
 //
 //  Created by Uwais Alqadri on 01/12/20.
 //
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Image("bg")
-                .resizable()
-                .edgesIgnoringSafeArea(.all)
-            VStack(spacing: 20) {
-                Logo()
-                FormBox()
-            }
-        
+        NavigationView {
+            Form {
+                // Section Profile
+                Section() {
+                   NavigationLink(destination: About()){
+                    HStack {
+                        Image("profile")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .clipShape(Circle())
+                        
+                        // Nama & Status
+                        VStack(alignment: .leading) {
+                            Text("Uwais").font(.headline)
+                            Text("Mobile Developer").font(.caption)
+                        }
+                    }
+                    .padding(.top, 10)
+                    .padding(.bottom, 10)
+                    
+                   }
+                }
+                
+                // section pengaturan umum
+                Section(header: Text("Pengaturan Umum").textCase(.none)) {
+                    NavigationLink(destination: Pesan()){
+                        HStack(spacing: 20) {
+                            Image(systemName: "star.fill")
+                                .frame(width: 35, height: 35)
+                                .background(Color.orange)
+                                .cornerRadius(10)
+                                .foregroundColor(.white)
+                        
+                            Text("Pesan Berbintang")
+                        }
+                    }
+                    
+                    NavigationLink(destination: About()) {
+                        HStack(spacing: 20) {
+                            Image(systemName: "tv")
+                                .frame(width: 35, height: 35)
+                                .background(Color.green)
+                                .cornerRadius(10)
+                                .foregroundColor(.white)
+                            
+                            Text("Whatsapp Web/Dekstop")
+                        }
+                    }
+                }
+                
+                // section pengaturan akun
+                Section(header:
+                        Text("Pengaturan Akun").textCase(.none)){
+                    NavigationLink(destination: About()) {
+                        HStack(spacing: 20) {
+                            Image(systemName: "person")
+                                .frame(width: 35, height: 35)
+                                .background(Color.blue)
+                                .cornerRadius(10)
+                                .foregroundColor(.white)
+                            
+                            Text("Akun")
+                        }
+                    }
+                    NavigationLink(destination: About()) {
+                        HStack(spacing: 20) {
+                            Image(systemName: "phone.circle")
+                                .frame(width: 35, height: 35)
+                                .background(Color.green)
+                                .cornerRadius(10)
+                                .foregroundColor(.white)
+                            
+                            Text("Chat")
+                        }
+                    }
+                }
+            }.navigationTitle("Settings")
         }
-//        padding(.all, 20)
     }
 }
 
@@ -29,53 +95,14 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct Logo: View {
+struct About: View {
     var body: some View {
-        
-       VStack{
-        Image("swift")
-            .resizable()
-            .frame(width: 100, height: 100)
-            .foregroundColor(.white)
-            .cornerRadius(20)
-        
-        Text("Halo SwiftUI").foregroundColor(Color.white)
-       }
+        Text("Halaman About")
     }
 }
 
-struct FormBox: View {
-    
-    @State var username: String = ""
-    @State var password: String = ""
-    
-    
+struct Pesan: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Username").font(.callout).bold()
-            TextField("Username...", text:$username)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-            
-            Text("Password").font(.callout).bold()
-            SecureField("Password...", text:$password)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-            
-            Button(action: {print("Hello Button")}) {
-               HStack{
-                Text("Sign in")
-                    .foregroundColor(.white)
-                    .font(.callout).bold()
-            
-                Spacer()
-               }
-            }
-            
-            .padding()
-            .background(Color.black)
-            .cornerRadius(10)
-        }
-        .padding(.all, 30)
-        .background(Color.orange)
-        .cornerRadius(10)
+        Text("Pesan Berbintang")
     }
 }
