c6cac74b10466fd7e281a8f8bcb73e599a618949
uwaisalqadri <uwaisalqadri654321@gmail.com>
2020-12-01 13:39:55 +0700

    initial

--- SwiftUITutorial/ContentView.swift
+++ SwiftUITutorial/ContentView.swift
@@ -9,13 +9,73 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("bg")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack(spacing: 20) {
+                Logo()
+                FormBox()
+            }
+        
+        }
+//        padding(.all, 20)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Logo: View {
+    var body: some View {
+        
+       VStack{
+        Image("swift")
+            .resizable()
+            .frame(width: 100, height: 100)
+            .foregroundColor(.white)
+            .cornerRadius(20)
+        
+        Text("Halo SwiftUI").foregroundColor(Color.white)
+       }
+    }
+}
+
+struct FormBox: View {
+    
+    @State var username: String = ""
+    @State var password: String = ""
+    
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Username").font(.callout).bold()
+            TextField("Username...", text:$username)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Text("Password").font(.callout).bold()
+            SecureField("Password...", text:$password)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Button(action: {print("Hello Button")}) {
+               HStack{
+                Text("Sign in")
+                    .foregroundColor(.white)
+                    .font(.callout).bold()
+            
+                Spacer()
+               }
+            }
+            
+            .padding()
+            .background(Color.black)
+            .cornerRadius(10)
+        }
+        .padding(.all, 30)
+        .background(Color.orange)
+        .cornerRadius(10)
     }
 }
