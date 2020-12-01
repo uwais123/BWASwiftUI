3b0d4f8095f4c7a4c9e812ca871e09b31c93d2e8
uwaisalqadri <uwaisalqadri654321@gmail.com>
2020-12-01 12:46:02 +0700

    Initial Commit

---
+++ SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by Uwais Alqadri on 01/12/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
